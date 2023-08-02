9272fb6828521b37a4c4bfe20213c6ada38d5680
Lukakhutornoi <lukakhutornoi08@gmail.com>
2023-08-02 21:19:27 +0400

    commit4

---
+++ main.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.querySelector("nav")
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+
+
+
